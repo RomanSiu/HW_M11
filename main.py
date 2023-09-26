ab7f90a75c4988ada5237731a5200d9babaddbbd
RomanSiu <siusiailoroman@gmail.com>
2023-09-26 21:09:11 +0300

    0.2.1

--- main.py
+++ main.py
@@ -134,6 +134,7 @@ class AddressBook(UserDict):
     def delete(self, name):        
         self.data.pop(name)
 
+    @input_error
     def iterator(self, n_on_page):
         list_notes = []
         for v in self.data.values():
@@ -145,38 +146,3 @@ class AddressBook(UserDict):
             if len(list_notes) == n_on_page:
                 yield list_notes
                 list_notes = []   
-                
-# Створення нової адресної книги
-book = AddressBook()
-
-# Створення запису для John
-john_record = Record("John")
-john_record.add_phone("1234567890", "01.04.1998")
-john_record.add_phone("5555555555")
-
-print(john_record.days_to_birthday())
-
-# Додавання запису John до адресної книги
-book.add_record(john_record)
-
-# Створення та додавання нового запису для Jane
-jane_record = Record("Jane")
-jane_record.add_phone("9876543210")
-book.add_record(jane_record)
-
-# Виведення всіх записів у книзі
-for name, record in book.data.items():
-    print(record)
-
-# Знаходження та редагування телефону для John
-john = book.find("John")
-john.edit_phone("1234567890", "1112223333")
-
-print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
-
-# Пошук конкретного телефону у записі John
-found_phone = john.find_phone("5555555555")
-print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
-
-# Видалення запису Jane
-book.delete("Jane")
